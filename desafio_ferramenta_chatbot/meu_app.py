b28cf9a6979a40cccf13474566759e112b53c4b1
Matheus Henrique Alves da Silva <98499413+teussilva@users.noreply.github.com>
2025-04-29 23:59:54 +0000

    codigo atualizado

--- desafio_ferramenta_chatbot/meu_app.py
+++ desafio_ferramenta_chatbot/meu_app.py
@@ -13,10 +13,10 @@ if botao_analise_sentimento:
     if len(user_text) > 0:
         scores = sia.polarity_scores(user_text)
         compound_score = scores['compound']
+        neg_score = scores['neg']
 
         st.subheader('Resultado da Análise:')
-        st.write(f'Pontuação (Compound): {compound_score:.2f}')
-
+        st.write(f'Pontuação (Composto): {compound_score:.2f}%')
         if compound_score >= 0.05:
             st.write('Sentimento: Positivo')
         elif compound_score <= -0.05:
@@ -25,3 +25,5 @@ if botao_analise_sentimento:
             st.write('Sentimento: Neutro')
     else:
         st.warning('Por favor, digite algum texto para analisar.')
+else:
+     st.warning('Nenhum texto inserido, Por favor preencha o campo obrigatório!!!')
